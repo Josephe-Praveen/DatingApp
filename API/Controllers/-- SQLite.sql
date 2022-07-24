49d03f6a870cc06a53b7c42bb7f714c3dd644a97
Josephe-Praveen <josephaws619@gmail.com>
2022-07-24 23:30:21 +0530

    End of Section 2

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,7 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1 , "Joseph");
+INSERT INTO Users (Id, UserName)
+VALUES (2 , "Praveen");
+INSERT INTO Users (Id, UserName)
+VALUES (3 , "Pajanore");
